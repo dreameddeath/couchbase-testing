7f16308c4afa66a2653d4176f24669fb6f9fd6b0
dreammedeath <christophejeunesse@hotmail.com>
2014-11-17 16:58:22 +0100

    travis attempt #7

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -ex
-wget https://protobuf.googlecode.com/files/protobuf-${PROTOBUF-VERSION}.tar.gz
-tar -xzvf protobuf-${PROTOBUF-VERSION}.tar.gz
-cd protobuf-${PROTOBUF-VERSION} && ./configure --prefix=/usr && make && sudo make install
+wget https://protobuf.googlecode.com/files/protobuf-${PROTOBUF_VERSION}.tar.gz
+tar -xzvf protobuf-${PROTOBUF_VERSION}.tar.gz
+cd protobuf-${PROTOBUF_VERSION} && ./configure --prefix=/usr && make && sudo make install
