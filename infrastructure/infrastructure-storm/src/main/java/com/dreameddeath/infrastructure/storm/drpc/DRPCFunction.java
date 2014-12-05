a54c8be387f238fa65bd18835fe9795af3d997ed
dreammedeath <christophejeunesse@hotmail.com>
2014-12-06 00:10:39 +0100

    Corrections around dhpc bolt external RPC

--- infrastructure/infrastructure-storm/src/main/java/com/dreameddeath/infrastructure/storm/drpc/DRPCFunction.java
+++ infrastructure/infrastructure-storm/src/main/java/com/dreameddeath/infrastructure/storm/drpc/DRPCFunction.java
@@ -1,7 +1,11 @@
 package com.dreameddeath.infrastructure.storm.drpc;
 
 
+import backtype.storm.generated.DistributedRPC;
 import backtype.storm.utils.DRPCClient;
+import backtype.storm.utils.ServiceRegistry;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import storm.trident.operation.BaseFunction;
@@ -9,14 +13,17 @@ import storm.trident.operation.TridentCollector;
 import storm.trident.operation.TridentOperationContext;
 import storm.trident.tuple.TridentTuple;
 
+import java.util.List;
 import java.util.Map;
 
 /**
  * Created by CEAJ8230 on 02/12/2014.
  */
-public abstract class DRPCFunction<T> extends BaseFunction {
+public abstract class DRPCFunction<TIN,TOUT> extends BaseFunction {
+    private static ObjectMapper MAPPER = new ObjectMapper();
+
     private static Logger LOG = LoggerFactory.getLogger(DRPCFunction.class);
-    private DRPCClient _drpcClient;
+    private DistributedRPC.Iface _drpcClient;
     private String _drpcServerName;
     private String _drpcFunctionName;
 
@@ -32,20 +39,29 @@ public abstract class DRPCFunction<T> extends BaseFunction {
         boolean isLocal = "local".equals(conf.get("storm.cluster.mode"));
         String serverHost = (String)conf.get(builderServerConfigEntry(_drpcServerName,EntryType.SERVER_NAME));
         Integer serverPort = Integer.parseInt((String) conf.get(builderServerConfigEntry(_drpcServerName, EntryType.SERVER_PORT)));
-        //LG.warn("isLocal {}",isLocal);
-        /*if(isLocal) {
-            _drpcClient = new DRPCClient("localhost", 3773);//drpc.invocations.port
-        }*/
-        if(isLocal){
-            _drpcClient = new DRPCClient(serverHost, serverPort);
+        String localServiceId = (String)conf.get(builderServerConfigEntry(_drpcServerName,EntryType.LOCAL_SERVICE_ID));
+        LOG.warn("isLocal {} with service id {}",isLocal,localServiceId);
+        if(isLocal) {
+            _drpcClient =  (DistributedRPC.Iface) ServiceRegistry.getService(localServiceId);
+        }
+        else{
+            _drpcClient = new DRPCClient(serverHost,serverPort);
         }
     }
 
+    public abstract TIN DRPCInputPrepareProcess(TridentTuple tridentTuple,ObjectMapper mapper);
+    public abstract TypeReference<List<List<TOUT>>> getTypeReference();
 
     @Override
     public void execute(TridentTuple tridentTuple, TridentCollector tridentCollector) {
         try {
-            DRPCResultProcess((T) _drpcClient.execute(_drpcFunctionName, tridentTuple.getString(0)), tridentCollector);
+            TIN processedInput=DRPCInputPrepareProcess(tridentTuple,MAPPER);
+            String drpcStrInput = MAPPER.writeValueAsString(processedInput);
+            String outputResult = _drpcClient.execute(_drpcFunctionName,drpcStrInput);
+            List<List<TOUT>> parsingResult = MAPPER.readValue(outputResult,getTypeReference());
+
+            DRPCResultProcess(parsingResult.get(0).get(0), tridentCollector);
+
         }
         catch(Exception e){
             LOG.error("Error",e);
@@ -53,19 +69,21 @@ public abstract class DRPCFunction<T> extends BaseFunction {
         }
     }
 
-    public abstract void DRPCResultProcess(T output, TridentCollector collector);
+    public abstract void DRPCResultProcess(TOUT output, TridentCollector collector);
 
 
     public static String builderServerConfigEntry(String serverName,EntryType type){
         switch(type){
             case SERVER_NAME:return "drpc.server."+ serverName +".location";
             case SERVER_PORT:return "drpc.server."+ serverName +".port";
+            case LOCAL_SERVICE_ID:return "drpc.server."+ serverName +".local_service_id";
             default : throw new RuntimeException("Error parameter unknown "+type);
         }
     }
 
     public enum EntryType{
         SERVER_NAME,
-        SERVER_PORT
+        SERVER_PORT,
+        LOCAL_SERVICE_ID
     }
 }
--- infrastructure/infrastructure-storm/src/test/java/com/dreameddeath/infrastructure/storm/drpc/DRPCFunctionTest.java
+++ infrastructure/infrastructure-storm/src/test/java/com/dreameddeath/infrastructure/storm/drpc/DRPCFunctionTest.java
@@ -3,30 +3,26 @@ package com.dreameddeath.infrastructure.storm.drpc;
 import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.LocalDRPC;
-import backtype.storm.generated.TopologyInfo;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.simple.JSONAware;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import storm.trident.TridentTopology;
 import storm.trident.operation.BaseFunction;
-import storm.trident.operation.Function;
 import storm.trident.operation.TridentCollector;
 import storm.trident.tuple.TridentTuple;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class DRPCFunctionTest {
     public static class TestingClass implements JSONAware{
@@ -46,40 +42,58 @@ public class DRPCFunctionTest {
         }
     }
 
-    public static class ListResult extends ArrayList<ArrayList<TestingClass>>{}
-
     public static class TestStdFunction extends BaseFunction {
         private static Logger LOG = LoggerFactory.getLogger(TestStdFunction.class);
         @Override
         public void execute(TridentTuple tridentTuple, TridentCollector tridentCollector) {
-            //LOG.warn("Trident data size <{}>", tridentTuple.size());
-            //Map<String,TestingClass> map = new HashMap<>();
             String input = tridentTuple.getString(0);
-            //map.put(input,new TestingClass(input,input + " from DRPC"));
-            Values emitValue = new Values(new TestingClass(input,input + " from DRPC"));
-            LOG.warn("Trident data size <{}>", emitValue);
-            tridentCollector.emit(emitValue);
+            try {
+                TestingClass result = new ObjectMapper().readValue(tridentTuple.getString(0), TestingClass.class);
+                result.out+=" from DRPC";
+                Values emitValue = new Values(result);
+                tridentCollector.emit(emitValue);
+            }
+            catch(Exception e){
+                throw new RuntimeException("Error ",e);
+            }
         }
     }
 
-    public static class DRCPCallTestFunction extends DRPCFunction<String>{
-
-        public DRCPCallTestFunction(String drcpServerName, String drcpFunctionName) {
-            super(drcpServerName, drcpFunctionName);
+    public static class DRPCCallTestFunction extends DRPCFunction<TestingClass,TestingClass>{
+        public DRPCCallTestFunction(String drpcServerName, String drpcFunctionName) {
+            super(drpcServerName, drpcFunctionName);
         }
 
-        /*public DRCPCallTestFunction(String drcpServerName, String drcpFunctionName,LocalDRPC drpc) {
-            super(drcpServerName, drcpFunctionName,drpc);
-        }*/
+        @Override
+        public TestingClass DRPCInputPrepareProcess(TridentTuple tridentTupl,ObjectMapper mapper) {
+            try {
+                return mapper.readValue(tridentTupl.getString(0), TestingClass.class);
+            }
+            catch(Exception e){
+                throw new RuntimeException("Error during parsing of tuple <"+tridentTupl.toString()+">",e);
+            }
+        }
 
         @Override
-        public void DRPCResultProcess(String output, TridentCollector collector) {
-            collector.emit(new Values(output + " has worked"));
+        public TypeReference<List<List<TestingClass>>> getTypeReference() {
+            return new TypeReference<List<List<TestingClass>>>(){};
+        }
+
+
+        @Override
+        public void DRPCResultProcess(TestingClass output, TridentCollector collector) {
+            try {
+                output.out += " has worked";
+                collector.emit(new Values(output));
+            }
+            catch(Exception e){
+                throw new RuntimeException("error during output submit",e);
+            }
         }
     }
 
     @Test
-    public void testDRCP(){
+    public void testDRPC(){
         LocalDRPC drpc = new LocalDRPC();
         LocalCluster cluster = new LocalCluster();
 
@@ -87,24 +101,41 @@ public class DRPCFunctionTest {
         conf.setDebug(true);
         conf.put(DRPCFunction.builderServerConfigEntry("test", DRPCFunction.EntryType.SERVER_NAME),"toto.tutu.com");
         conf.put(DRPCFunction.builderServerConfigEntry("test", DRPCFunction.EntryType.SERVER_PORT),"1023");
+        conf.put(DRPCFunction.builderServerConfigEntry("test", DRPCFunction.EntryType.LOCAL_SERVICE_ID),drpc.getServiceId());
         TridentTopology topology = new TridentTopology();
         topology.newDRPCStream("testSimpleFunction",drpc).parallelismHint(3).each(new Fields("args"), new TestStdFunction(), new Fields("result")).project(new Fields("result"));
         cluster.submitTopology("testSimpleFunction", conf, topology.build());
 
-        /*TridentTopology topologyDRCP = new TridentTopology();
-        topologyDRCP.newDRPCStream("testDRCP",drpc).each(
+        TridentTopology topologyDRPC = new TridentTopology();
+        topologyDRPC.newDRPCStream("testDRPCFunction", drpc).each(
                 new Fields("args"),
-                new DRCPCallTestFunction("test", "testSimpleFunction"), new Fields("result")).project(new Fields("result"));
-        cluster.submitTopology("testDRCPFunction", conf, topologyDRCP.build());*/
-        String result = drpc.execute("testSimpleFunction","the value submitted");
+                new DRPCCallTestFunction("test", "testSimpleFunction"), new Fields("result")).project(new Fields("result"));
+        cluster.submitTopology("testDRPCFunction", conf, topologyDRPC.build());
 
-        System.out.println("The found element is :"+result);
+        TestingClass input = new TestingClass();
+        input.in = "the intial value submitted";
+        input.out = "the intial value submitted";
+
         try {
-            ListResult parsingResult = new ObjectMapper().readValue(result,ListResult.class);
-            Object raw = new JSONParser().parse(result);
-            assertEquals("the value submitted from DRPC",parsingResult.get(0).get(0).out);
+            //String result = drpc.execute("testSimpleFunction",new ObjectMapper().writeValueAsString(input));
+
+            String result2 = drpc.execute("testDRPCFunction",new ObjectMapper().writeValueAsString(input));
+            System.out.println(result2);
+
+
+
+
+            System.out.println("The found element is :"+result2);
+
+            //ListResult parsingResult = new ObjectMapper().readValue(result,ListResult.class);
+            List<List<TestingClass>> parsingResult = new ObjectMapper().readValue(
+                    result2,
+                    new TypeReference<List<List<TestingClass>>>() {}
+                );
+            //Object raw = new JSONParser().parse(result);
+            assertEquals("the intial value submitted from DRPC has worked",parsingResult.get(0).get(0).out);
         }
-        catch(ParseException|IOException e){
+        catch(IOException e){
             fail("error during parsing :"+e.getMessage());
         }
     }
