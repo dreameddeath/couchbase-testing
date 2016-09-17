58e6a337ce37ce9bf2535c8a6088cf710b7ff9d6
dreammedeath <christophejeunesse@hotmail.com>
2016-09-17 12:36:12 +0200

    Change proto buf download source

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 if [ ! -d "$HOME/protobuf/lib" ]; then
-    wget https://protobuf.googlecode.com/files/protobuf-${PROTOBUF_VERSION}.tar.gz
+    wget https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/protobuf-${PROTOBUF_VERSION}.tar.gz
     tar -xzvf protobuf-${PROTOBUF_VERSION}.tar.gz
     cd protobuf-${PROTOBUF_VERSION} && ./configure --prefix=${HOME}/protobuf && make && make install
 else
