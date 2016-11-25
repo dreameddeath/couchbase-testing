9471cb6340190d2970e61012b4f3b6794f4a627a
dreammedeath <christophejeunesse@hotmail.com>
2016-11-25 09:34:18 +0100

    True async call mode

--- core/core-service/src/main/java/com/dreameddeath/core/service/client/rest/rxjava/RxJavaInvoker.java
+++ core/core-service/src/main/java/com/dreameddeath/core/service/client/rest/rxjava/RxJavaInvoker.java
@@ -18,19 +18,21 @@
 
 package com.dreameddeath.core.service.client.rest.rxjava;
 
+import rx.AsyncEmitter;
 import rx.Observable;
-import rx.schedulers.Schedulers;
 
+import javax.ws.rs.HttpMethod;
 import javax.ws.rs.client.AsyncInvoker;
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.InvocationCallback;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
-import java.util.concurrent.Future;
 
 /**
  * Created by Christophe Jeunesse on 17/09/2016.
  */
 public class RxJavaInvoker {
+    public static final String HTTP_METHOD_TRACE = "TRACE";
     private final AsyncInvoker origInvoker;
 
     public RxJavaInvoker(AsyncInvoker origInvoker) {
@@ -38,110 +40,137 @@ public class RxJavaInvoker {
     }
 
     public Observable<Response> get() {
-        return build(origInvoker.get());
+        return method(HttpMethod.GET);
     }
 
     public <T> Observable<T> get(Class<T> responseType) {
-        return build(origInvoker.get(responseType));
+        return method(HttpMethod.GET,responseType);
     }
     
     public <T> Observable<T> get(GenericType<T> responseType) {
-        return build(origInvoker.get(responseType));
+        return method(HttpMethod.GET,responseType);
     }
 
     public Observable<Response> put(Entity<?> entity) {
-        return build(origInvoker.put(entity));
+        return method(HttpMethod.PUT,entity);
     }
 
     public <T> Observable<T> put(Entity<?> entity, Class<T> responseType) {
-        return build(origInvoker.put(entity,responseType));
+        return method(HttpMethod.PUT,entity,responseType);
     }
 
     public <T> Observable<T> put(Entity<?> entity, GenericType<T> responseType) {
-        return build(origInvoker.put(entity,responseType));
+        return method(HttpMethod.PUT,entity,responseType);
     }
 
     public Observable<Response> post(Entity<?> entity) {
-        return build(origInvoker.post(entity));
+        return method(HttpMethod.POST,entity);
     }
 
     public <T> Observable<T> post(Entity<?> entity, Class<T> responseType) {
-        return build(origInvoker.post(entity,responseType));
+        return method(HttpMethod.POST,entity,responseType);
     }
 
     public <T> Observable<T> post(Entity<?> entity, GenericType<T> responseType) {
-        return build(origInvoker.post(entity,responseType));
+        return method(HttpMethod.POST,entity,responseType);
     }
 
     public Observable<Response> delete() {
-        return build(origInvoker.delete());
+        return method(HttpMethod.DELETE);
     }
 
     public <T> Observable<T> delete(Class<T> responseType) {
-        return build(origInvoker.delete(responseType));
+        return method(HttpMethod.DELETE,responseType);
     }
 
     public <T> Observable<T> delete(GenericType<T> responseType) {
-        return build(origInvoker.delete(responseType));
+        return method(HttpMethod.DELETE,responseType);
     }
 
     public Observable<Response> head() {
-        return build(origInvoker.head());
+        return method(HttpMethod.HEAD);
     }
 
     public Observable<Response> options() {
-        return build(origInvoker.options());
+        return method(HttpMethod.OPTIONS);
     }
 
     public <T> Observable<T> options(Class<T> responseType) {
-        return build(origInvoker.options(responseType));
+        return method(HttpMethod.OPTIONS,responseType);
     }
 
     public <T> Observable<T> options(GenericType<T> responseType) {
-        return build(origInvoker.options(responseType));
+        return method(HttpMethod.OPTIONS,responseType);
     }
 
     public Observable<Response> trace() {
-        return build(origInvoker.trace());
+        return method(HTTP_METHOD_TRACE);
     }
 
     public <T> Observable<T> trace(Class<T> responseType) {
-        return build(origInvoker.trace(responseType));
+        return method(HTTP_METHOD_TRACE,responseType);
     }
 
     public <T> Observable<T> trace(GenericType<T> responseType) {
-        return build(origInvoker.trace(responseType));
+        return method(HTTP_METHOD_TRACE,responseType);
     }
 
 
     public Observable<Response> method(String name) {
-        return build(origInvoker.method(name));
+        return method(name,null,(GenericType<Response>)null);
     }
 
     public <T> Observable<T> method(String name, Class<T> responseType) {
-        return build(origInvoker.method(name,responseType));
+        return method(name,null,responseType);
     }
 
     public <T> Observable<T> method(String name, GenericType<T> responseType) {
-        return build(origInvoker.method(name,responseType));
+        return method(name,null,responseType);
     }
 
 
     public Observable<Response> method(String name, Entity<?> entity) {
-        return build(origInvoker.method(name,entity));
+        return method(name,entity,(GenericType<Response>)null);
     }
 
     public <T> Observable<T> method(String name, Entity<?> entity, Class<T> responseType) {
-        return build(origInvoker.method(name,entity,responseType));
+        return method(name,entity,new GenericType<>(responseType));
     }
 
 
     public <T> Observable<T> method(String name, Entity<?> entity, GenericType<T> responseType) {
-        return build(origInvoker.method(name,entity,responseType));
+        return buildObservableInvocation(name,entity,responseType);
+    }
+
+    private <T> Observable<T> buildObservableInvocation(final String method, final Entity<?> entity, final GenericType<T> responseType){
+        return Observable.fromAsync(emitter->{
+                InvocationCallback<Response> callback = new InvocationCallback<Response>(){
+                    @Override
+                    public void completed(Response o) {
+                        if(responseType==null){
+                            emitter.onNext((T) o);
+                        }
+                        else {
+                            try {
+                                emitter.onNext(o.readEntity(responseType));
+                            } catch (Throwable e) {
+                                emitter.onError(e);
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void failed(Throwable throwable) {
+                        emitter.onError(throwable);
+                    }
+                };
+                if(entity==null){origInvoker.method(method,callback);}
+                else{origInvoker.method(method,entity,callback);}
+        },
+                AsyncEmitter.BackpressureMode.ERROR
+        );
     }
 
 
-    private <T> Observable<T> build(Future<T> future){
-        return Observable.from(future).subscribeOn(Schedulers.io());
-    }
+
 }
